fdfc5399602559058b9989f8cc0af1734f858885
Horckun <horckun@gmail.com>
2020-05-04 18:06:27 +0200

    100_SagaOfTheMaestro
    Archivos traducidos de quest Maestro en L2jHorckun\L2jFrozen_DataPack\bin\quests\100_SagaOfTheMaestro

--- L2jFrozen_DataPack/bin/quests/100_SagaOfTheMaestro/__init__.py
+++ L2jFrozen_DataPack/bin/quests/100_SagaOfTheMaestro/__init__.py
@@ -23,13 +23,13 @@ class Quest (JQuest) :
      self.X = [164650,47429,47391]
      self.Y = [-74121,-56923,-56929]
      self.Z = [-2871,-2383,-2370]
-     self.Text = ["PLAYERNAME! Pursued to here! However, I jumped out of the Banshouren boundaries! You look at the giant as the sign of power!",
-                  "... Oh ... good! So it was ... let's begin!","I do not have the patience ..! I have been a giant force ...! Cough chatter ah ah ah!",
-                  "Paying homage to those who disrupt the orderly will be PLAYERNAME's death!","Now, my soul freed from the shackles of the millennium, Halixia, to the back side I come ...",
-                  "Why do you interfere others' battles?","This is a waste of time.. Say goodbye...!","...That is the enemy",
-                  "...Goodness! PLAYERNAME you are still looking?","PLAYERNAME ... Not just to whom the victory. Only personnel involved in the fighting are eligible to share in the victory.",
-                  "Your sword is not an ornament. Don't you think, PLAYERNAME?","Goodness! I no longer sense a battle there now.","let...","Only engaged in the battle to bar their choice. Perhaps you should regret.",
-                  "The human nation was foolish to try and fight a giant's strength.","Must...Retreat... Too...Strong.","PLAYERNAME. Defeat...by...retaining...and...Mo...Hacker","....! Fight...Defeat...It...Fight...Defeat...It..."]
+     self.Text = ["PLAYERNAME! Perseguido hasta aqui! Sin embargo, salte de los limites de Banshouren. Miras al gigante como el signo del poder!",
+                  "... Oh Dios! Asi que fue... comencemos!","No tengo paciencia...! He sido una fuerza gigante...! Charla de tos ah ah ah!",
+                  "Rendir homenaje a los que interrumpen a los ordenados sera la muerte de PLAYERNAME!","Ahora, mi alma liberada de los grilletes del milenio, Halixia, a la parte de atras vengo...",
+                  "Por que interfieres en las batallas de los demas?","Esto es una perdida de tiempo... Di adios...! ","... Ese es el enemigo.",
+                  "...Bondad! PLAYERNAME que todavia estas buscando?","PLAYERNAME... No solo a quien la victoria. Solo el personal involucrado en la lucha es elegible para compartir la victoria.",
+                  "Tu espada no es un adorno. No crees, PLAYERNAME?","Dios mio! Ya no siento una batalla alli ahora","deja...","Solo participe en la batalla para prohibir su eleccion. Quizas deberias arrepentirte.",
+                  "La nacion humana era tonta al tratar de luchar contra la fuerza de un gigante.","Debe... Retirarse... Demasiado... Fuerte","PLAYERNAME. Derrota... reteniendo... y... Mo... Hacker ","....! Lucha... Derrota... Es... Lucha... Derrota... Es..."]
      # finally, register all events to be triggered appropriately, using the overriden values.
      JQuest.registerNPCs(self)
 
