2a8fff1404f593ec257e7a42cdc3770f3e497d55
Horckun <horckun@gmail.com>
2020-09-30 13:26:10 +0200

    12_SecretMeetingWithVarkaSilenos
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\12_SecretMeetingWithVarkaSilenos

--- L2jFrozen_DataPack/bin/quests/12_SecretMeetingWithVarkaSilenos/__init__.py
+++ L2jFrozen_DataPack/bin/quests/12_SecretMeetingWithVarkaSilenos/__init__.py
@@ -45,7 +45,7 @@ class Quest (JQuest) :
 
  def onTalk (self,npc,player):
      npcId = npc.getNpcId()
-     htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+     htmltext = "<html><body>O no estas llevando a cabo su busqueda o no cumples con los criterios.</body></html>"
      st = player.getQuestState(qn)
      if not st : return htmltext
 
