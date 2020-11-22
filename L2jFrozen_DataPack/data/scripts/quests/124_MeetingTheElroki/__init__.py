c6dddd9a37e4d062308b3474a2234e5e0dc7b81d
Horckun <horckun@gmail.com>
2020-11-22 10:46:40 +0100

    124_MeetingTheElroki
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\data\scripts\quests\124_MeetingTheElroki

--- L2jFrozen_DataPack/data/scripts/quests/124_MeetingTheElroki/__init__.py
+++ L2jFrozen_DataPack/data/scripts/quests/124_MeetingTheElroki/__init__.py
@@ -58,7 +58,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-     htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+     htmltext = "<html><body>O no esta llevando a cabo su busqueda o no cumple con los criterios.</body></html>"
      st = player.getQuestState(qn)
      if not st : return htmltext
      npcId = npc.getNpcId()
@@ -68,7 +68,7 @@ class Quest (JQuest) :
      if id == CREATED :
          st.set("cond","0")
      elif id == COMPLETED :
-         htmltext = "<html><body>This quest has already been completed.</body></html>"
+         htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
      if npcId == MARQUEZ:
          if cond == 0 :
              if player.getLevel() < 75 :
