f4c3848149bb93172430cb359112bc4d2df750ac
Horckun <horckun@gmail.com>
2021-01-21 11:59:46 +0100

    14_WhereaboutsOfTheArchaeologist
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\14_WhereaboutsOfTheArchaeologist

--- L2jFrozen_DataPack/bin/quests/14_WhereaboutsOfTheArchaeologist/__init__.py
+++ L2jFrozen_DataPack/bin/quests/14_WhereaboutsOfTheArchaeologist/__init__.py
@@ -37,7 +37,7 @@ class Quest (JQuest) :
    return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo la busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
    npcId = npc.getNpcId()
@@ -45,7 +45,7 @@ class Quest (JQuest) :
    cond = st.getInt("cond")
    if npcId == LIESEL and cond == 0 :
      if id == COMPLETED :
-       htmltext = "<html><body>This quest has already been completed.</body></html>"
+       htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
      elif player.getLevel() < 74 : 
        htmltext = "31263-1.htm"
        st.exitQuest(1)
