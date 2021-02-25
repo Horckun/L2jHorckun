2fc7bbd2ab2ca102d153142739f16dbc2e4032f5
Horckun <horckun@gmail.com>
2021-02-25 13:28:48 +0100

    151_SaveMySister1
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\151_SaveMySister1

--- L2jFrozen_DataPack/bin/quests/151_SaveMySister1/__init__.py
+++ L2jFrozen_DataPack/bin/quests/151_SaveMySister1/__init__.py
@@ -22,7 +22,7 @@ class Quest (JQuest) :
     return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo su busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -33,7 +33,7 @@ class Quest (JQuest) :
    med = st.getQuestItemsCount(FEVER_MEDICINE)
    if npcId == 30050 :
       if id == COMPLETED :
-        htmltext = "<html><body>This quest has already been completed.</body></html>"
+        htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
       elif cond == 0 :
         if player.getLevel() >= 15 :
           htmltext = "30050-02.htm"
