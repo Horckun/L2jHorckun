392c7f5de543ee8ba43428bdc815c64552a7b2fb
Horckun <horckun@gmail.com>
2021-03-18 13:24:13 +0100

    156_MillenniumLove
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\156_MillenniumLove

--- L2jFrozen_DataPack/bin/quests/156_MillenniumLove/__init__.py
+++ L2jFrozen_DataPack/bin/quests/156_MillenniumLove/__init__.py
@@ -42,14 +42,14 @@ class Quest (JQuest) :
     return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
    npcId = npc.getNpcId()
    id = st.getState()
    if id == COMPLETED :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
    elif npcId == 30368 :
       if not st.getInt("cond") :
          htmltext = "30368-04.htm"
