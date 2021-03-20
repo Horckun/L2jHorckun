f5ad27aa73cbdc101fd14153a420f3e06f720866
Horckun <horckun@gmail.com>
2021-03-20 10:39:04 +0100

    158_SeedOfEvil
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\158_SeedOfEvil

--- L2jFrozen_DataPack/bin/quests/158_SeedOfEvil/__init__.py
+++ L2jFrozen_DataPack/bin/quests/158_SeedOfEvil/__init__.py
@@ -25,7 +25,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -48,7 +48,7 @@ class Quest (JQuest) :
           htmltext = "30031-02.htm"
           st.exitQuest(1)
    elif npcId == 30031 and st.getInt("cond")==0 and st.getInt("onlyone")==1 :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
    elif npcId == 30031 and st.getInt("cond")!=0 and st.getQuestItemsCount(CLAY_TABLET_ID)==0 :
         htmltext = "30031-05.htm"
    elif npcId == 30031 and st.getInt("cond")!=0 and st.getQuestItemsCount(CLAY_TABLET_ID)!=0 and st.getInt("onlyone")==0 :
