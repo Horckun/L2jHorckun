f995a4f415b42762ddc33cfebed745eb85eafad1
Horckun <horckun@gmail.com>
2021-03-23 14:07:32 +0100

    160_NerupasFavor
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\160_NerupasFavor

--- L2jFrozen_DataPack/bin/quests/160_NerupasFavor/__init__.py
+++ L2jFrozen_DataPack/bin/quests/160_NerupasFavor/__init__.py
@@ -28,7 +28,7 @@ class Quest (JQuest) :
     return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -44,7 +44,7 @@ class Quest (JQuest) :
        htmltext = "30370-02.htm"
        st.exitQuest(1)
    elif id == COMPLETED :
-     htmltext = "<html><body>This quest has already been completed.</body></html>"
+     htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
    elif id == STARTED :
      try :
        cond = st.getInt("cond")
