c825c1defbbc0e3f695bbee51d66375a559207d3
Horckun <horckun@gmail.com>
2021-03-26 11:25:42 +0100

    164_BloodFiend
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\164_BloodFiend

--- L2jFrozen_DataPack/bin/quests/164_BloodFiend/__init__.py
+++ L2jFrozen_DataPack/bin/quests/164_BloodFiend/__init__.py
@@ -25,7 +25,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -50,7 +50,7 @@ class Quest (JQuest) :
         htmltext = "30149-02.htm"
         st.exitQuest(1)
    elif npcId == 30149 and st.getInt("cond")==0 and st.getInt("onlyone")==1 :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
    elif npcId == 30149 and st.getInt("cond") :
       if st.getQuestItemsCount(KIRUNAK_SKULL_ID)<1 :
         htmltext = "30149-05.htm"
