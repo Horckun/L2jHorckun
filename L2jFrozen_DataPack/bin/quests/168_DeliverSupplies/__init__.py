1b862176e1e757e3e7e807c7a5dd370dfce7a2ff
Horckun <horckun@gmail.com>
2021-04-01 11:25:37 +0200

    168_DeliverSupplies
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\168_DeliverSupplies

--- L2jFrozen_DataPack/bin/quests/168_DeliverSupplies/__init__.py
+++ L2jFrozen_DataPack/bin/quests/168_DeliverSupplies/__init__.py
@@ -30,7 +30,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -55,7 +55,7 @@ class Quest (JQuest) :
         htmltext = "30349-01.htm"
         st.exitQuest(1)
    elif npcId == 30349 and st.getInt("cond")==0 and st.getInt("onlyone")==1 :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
    elif npcId == 30349 and st.getInt("cond")==1 and st.getQuestItemsCount(JENNIES_LETTER_ID) :
         htmltext = "30349-04.htm"
    elif npcId == 30349 and st.getInt("cond")==1 and st.getQuestItemsCount(SENTRY_BLADE1_ID)==1 and st.getQuestItemsCount(SENTRY_BLADE2_ID)==1 and st.getQuestItemsCount(SENTRY_BLADE3_ID)==1 :
