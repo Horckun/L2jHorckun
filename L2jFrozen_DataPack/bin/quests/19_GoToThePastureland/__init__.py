800b216bd81c1aaf949ea8a6e8dcd029907cf791
Horckun <horckun@gmail.com>
2021-05-30 11:54:16 +0200

    19_GoToThePastureland
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\19_GoToThePastureland

--- L2jFrozen_DataPack/bin/quests/19_GoToThePastureland/__init__.py
+++ L2jFrozen_DataPack/bin/quests/19_GoToThePastureland/__init__.py
@@ -33,7 +33,7 @@ class Quest (JQuest) :
    return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -43,11 +43,11 @@ class Quest (JQuest) :
    if npcId == VLADIMIR :
      if cond == 0 :
        if id == COMPLETED :
-         htmltext = "<html><body>This quest has already been completed.</body></html>"
+         htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
        elif player.getLevel() >= 63 :
          htmltext = "31302-0.htm"
        else:
-         htmltext = "<html><body>Quest for characters level 63 or above.</body></html>"
+         htmltext = "<html><body>Busqueda de personajes de nivel 63 o superior.</body></html>"
          st.exitQuest(1)
      else :
        htmltext = "31302-2.htm"
