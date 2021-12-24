60e0a274f76bd197f39e51f37d9ef5d738ee1dab
Horckun <horckun@gmail.com>
2021-12-24 19:26:21 +0100

    216_TrialOfGuildsman
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\216_TrialOfGuildsman

--- L2jFrozen_DataPack/bin/quests/216_TrialOfGuildsman/__init__.py
+++ L2jFrozen_DataPack/bin/quests/216_TrialOfGuildsman/__init__.py
@@ -125,7 +125,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -149,7 +149,7 @@ class Quest (JQuest) :
             htmltext = "30103-01.htm"
             st.exitQuest(1)
    elif npcId == 30103 and st.getInt("cond")==0 and st.getInt("onlyone")==1 :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<html><body>TEsta mision ya se ha completado.</body></html>"
    elif npcId == 30103 and st.getInt("cond")>=1 and st.getQuestItemsCount(VALKONS_RECOMMEND_ID)==1 :
         htmltext = "30103-07.htm"
    elif npcId == 30103 and st.getInt("cond")>=1 and st.getQuestItemsCount(ALLTRANS_INSTRUCTIONS_ID)==1 :
