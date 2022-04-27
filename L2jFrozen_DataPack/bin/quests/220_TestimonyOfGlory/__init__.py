a8ccca7504d9294122f1af68a515954f85e283e2
Horckun <horckun@gmail.com>
2022-04-27 23:36:43 +0200

    220_TestimonyOfGlory
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\220_TestimonyOfGlory

--- L2jFrozen_DataPack/bin/quests/220_TestimonyOfGlory/__init__.py
+++ L2jFrozen_DataPack/bin/quests/220_TestimonyOfGlory/__init__.py
@@ -181,7 +181,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<htm><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></htm>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -204,7 +204,7 @@ class Quest (JQuest) :
          st.exitQuest(1)
    elif id == COMPLETED :                                   # Check if the quest is already made
       if npcId == 30514 :
-        htmltext = "<html><body>This quest has already been completed.</body></html>"
+        htmltext = "<htm><body>Esta busqueda ya ha sido completada.</body></htm>"
    else :                                                   # The quest it self
      try :
        cond = st.getInt("cond")
@@ -495,7 +495,7 @@ class Quest (JQuest) :
        st.playSound("Itemsound.quest_before_battle")
        #st.addSpawn(27086)
        st.addSpawn(27086, 11839,-106261,-3550,300000)
-       return "Revenant of Tantos Chief has spawned at X=11839 Y=-106261 Z=-3550"
+       return "El renacido del jefe de Tantos ha engendrado en X=11839 Y=-106261 Z=-3550"
        # Alternate coord. set:
        #st.addSpawn(27086,11567,-106785,-3520)
      elif npcId == 27086 :
