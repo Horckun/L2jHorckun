75ac81b8ad914a541cfd7c88b13a3f0b3bafd9d2
Horckun <horckun@gmail.com>
2022-09-26 14:44:55 +0200

    228_TestOfMagus
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\228_TestOfMagus

--- L2jFrozen_DataPack/bin/quests/228_TestOfMagus/__init__.py
+++ L2jFrozen_DataPack/bin/quests/228_TestOfMagus/__init__.py
@@ -104,7 +104,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<htm><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></htm>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -132,7 +132,7 @@ class Quest (JQuest) :
           htmltext = "30629-01.htm"
           st.exitQuest(1)
      elif st.getInt("cond")==0 and st.getInt("onlyone")==1 :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<htm><body>Esta busqueda ya ha sido completada.</body></htm>"
      elif st.getInt("cond")==1:
         htmltext = "30629-05.htm"
      elif st.getInt("cond")==2:
