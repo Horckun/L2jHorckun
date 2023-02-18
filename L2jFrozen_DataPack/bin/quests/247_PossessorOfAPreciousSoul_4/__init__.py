b1a96de7d26884ab6e0823c92febb0899eaf312b
Horckun <horckun@gmail.com>
2023-02-18 19:59:55 +0100

    247_PossessorOfAPreciousSoul_4
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\247_PossessorOfAPreciousSoul_4

--- L2jFrozen_DataPack/bin/quests/247_PossessorOfAPreciousSoul_4/__init__.py
+++ L2jFrozen_DataPack/bin/quests/247_PossessorOfAPreciousSoul_4/__init__.py
@@ -57,7 +57,7 @@ class Quest (JQuest) :
    return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<htm><body>O no estas llevando a cabo tu busqueda o no cumples con los criterios.</body></htm>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -72,7 +72,7 @@ class Quest (JQuest) :
      if npcId == CARADINE and st.getQuestItemsCount(CARADINE_LETTER_LAST) == 1 :
        if cond in [0,1] :
          if id == COMPLETED :
-           htmltext = "<html><body>This quest has already been completed.</body></html>"
+           htmltext = "<htm><body>Esta busqueda ya ha sido completada.</body></htm>"
          elif player.getLevel() < 75 : 
            htmltext = "31740-2.htm"
            st.exitQuest(1)
