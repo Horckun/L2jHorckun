5b242930db5d60f686523737ab93572ee847b565
Horckun <horckun@gmail.com>
2023-05-31 23:07:19 +0200

    120_PavelsResearch
    Corrección en etiquetas en L2jHorckun\L2jFrozen_DataPack\bin\quests\120_PavelsResearch

--- L2jFrozen_DataPack/bin/quests/120_PavelsResearch/__init__.py
+++ L2jFrozen_DataPack/bin/quests/120_PavelsResearch/__init__.py
@@ -207,7 +207,11 @@ class Quest (JQuest) :
     return htmltext
 
  def onTalk (self,npc,player):
+<<<<<<< Updated upstream
     htmltext = "<html><head><body>O no estas en una mision que involucra a este NPC, o no cumples con los requisitos minimos de mision de este NPC.</body></html>"
+=======
+    htmltext = "<html><body>O no estas en una mision que involucra a este NPC, o no cumples con los requisitos minimos de mision de este NPC.</body></html>"
+>>>>>>> Stashed changes
     st = player.getQuestState(qn)
     if not st : return htmltext
     state = st.getState()
