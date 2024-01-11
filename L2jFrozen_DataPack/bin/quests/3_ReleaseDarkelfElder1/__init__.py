782cd74a3a40d2ef6bfa3be8012743a2e353950e
Horckun <horckun@gmail.com>
2024-01-11 09:28:41 +0100

    3_ReleaseDarkelfElder1
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\3_ReleaseDarkelfElder1

--- L2jFrozen_DataPack/bin/quests/3_ReleaseDarkelfElder1/__init__.py
+++ L2jFrozen_DataPack/bin/quests/3_ReleaseDarkelfElder1/__init__.py
@@ -36,7 +36,7 @@ class Quest (JQuest) :
    return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu mision o no cumples los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -44,7 +44,7 @@ class Quest (JQuest) :
    id = st.getState()
    cond = st.getInt("cond")
    if id == COMPLETED :
-      htmltext = "<html><body>This quest has already been completed.</body></html>"
+      htmltext = "<html><body>Esta mision ya se ha completado.</body></html>"
    elif cond == 0 :
       if player.getRace().ordinal() != 2 :
          htmltext = "30141-00.htm"
