12a2329be8b2b2c09149512441a0da96cb89c2ee
Horckun <horckun@gmail.com>
2024-04-19 16:44:56 +0200

    34_InSearchOfClothes
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\34_InSearchOfClothes

--- L2jFrozen_DataPack/bin/quests/34_InSearchOfClothes/__init__.py
+++ L2jFrozen_DataPack/bin/quests/34_InSearchOfClothes/__init__.py
@@ -46,11 +46,11 @@ class Quest (JQuest) :
        st.playSound("ItemSound.quest_finish")
        st.exitQuest(1)
      else :
-       htmltext = "You don't have enough materials"
+       htmltext = "No tienes suficientes materiales"
    return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu mision o no cumples los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
    npcId = npc.getNpcId()
@@ -76,7 +76,7 @@ class Quest (JQuest) :
          htmltext = "30165-2.htm"
        elif npcId == 30088 and cond == 6 :
           htmltext = "30088-4.htm"
-       else : htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>" 
+       else : htmltext = "<html><body>O no estas llevando a cabo tu mision o no cumples los criterios.</body></html>" 
    return htmltext
 
  def onKill(self,npc,player,isPet):
