9b3e0ab831b1d6de6e360a18a20f7e82af934a82
Horckun <horckun@gmail.com>
2024-06-04 21:20:03 +0200

    355_FamilyHonor
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\355_FamilyHonor

--- L2jFrozen_DataPack/bin/quests/355_FamilyHonor/__init__.py
+++ L2jFrozen_DataPack/bin/quests/355_FamilyHonor/__init__.py
@@ -70,7 +70,7 @@ class Quest (JQuest) :
    return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu mision o no cumples los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -101,7 +101,7 @@ class Quest (JQuest) :
      if st.getQuestItemsCount(WORK_OF_BERONA) :   
        htmltext = "30929-0.htm"
      else :
-       htmltext = "<html><body>You have nothing to appraise.</body></html>"
+       htmltext = "<html><body>No tienes nada que valorar.</body></html>"
    return htmltext
 
  def onKill(self,npc,player,isPet):
