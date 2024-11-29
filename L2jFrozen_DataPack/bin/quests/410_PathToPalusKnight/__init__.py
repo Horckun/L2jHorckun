e64430fc7a457acf93e4c1210e353229f92f97d3
Horckun <horckun@gmail.com>
2024-11-29 20:33:15 +0100

    410_PathToPalusKnight
    Archivos traducidos en L2jHorckun\L2jFrozen_DataPack\bin\quests\410_PathToPalusKnight

--- L2jFrozen_DataPack/bin/quests/410_PathToPalusKnight/__init__.py
+++ L2jFrozen_DataPack/bin/quests/410_PathToPalusKnight/__init__.py
@@ -67,7 +67,7 @@ class Quest (JQuest) :
 
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>O no estas llevando a cabo tu mision o no cumples los criterios.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
@@ -76,7 +76,7 @@ class Quest (JQuest) :
    if npcId != 30329 and id != STARTED : return htmltext
 
    npcId = npc.getNpcId()
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>" 
+   htmltext = "<html><body>O no estas llevando a cabo tu mision o no cumples los criterios.</body></html>" 
    id = st.getState()
    if id == CREATED :
      st.setState(STARTING)
